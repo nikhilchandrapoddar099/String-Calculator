1126a9150421b3e0c6607ffd0f3fd6e74d97a2c9
nikhilchandrapoddar099 <nikhilchandrapoddar099@gmail.com>
2025-01-17 21:24:51 +0530

    Handled single number input

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -1,5 +1,5 @@
 import { add } from "../src/stringCalculator";
 
-test("should return 0 for an empty string", () => {
-  expect(add("")).toBe(0);
+test("should return the number itself for a single number", () => {
+  expect(add("1")).toBe(1);
 });
