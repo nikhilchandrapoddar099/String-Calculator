c2b52ab90ce00ae740e3f09d69eab32098e50564
nikhilchandrapoddar099 <nikhilchandrapoddar099@gmail.com>
2025-01-17 22:39:44 +0530

    Support multiple delimiters with length longer than one char

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -1,5 +1,5 @@
 import { add } from "../src/stringCalculator";
 
-test("should support multiple custom delimiters", () => {
-  expect(add("//[*][%]\n1*2%3")).toBe(6);
+test("should support multiple delimiters with length > 1", () => {
+  expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
 });
