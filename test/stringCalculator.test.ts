3755392c03def687cb09187289115b546b2b8795
nikhilchandrapoddar099 <nikhilchandrapoddar099@gmail.com>
2025-01-17 21:29:31 +0530

    Tested with multiple no of strings

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -1,5 +1,5 @@
 import { add } from "../src/stringCalculator";
 
-test("should return the sum of two numbers separated by a comma", () => {
-  expect(add("1,5")).toBe(6);
+test("should return the sum of multiple numbers separated by commas", () => {
+  expect(add("1,2,3,4,5")).toBe(15);
 });
